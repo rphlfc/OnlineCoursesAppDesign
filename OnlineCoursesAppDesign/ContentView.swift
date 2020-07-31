e88576b06e9c8c160796ffd42f6c762e7ff64b9a
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-07-31 14:43:37 -0300

    Initial Commit

---
+++ OnlineCoursesAppDesign/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  OnlineCoursesAppDesign
+//
+//  Created by Raphael Cerqueira on 31/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
